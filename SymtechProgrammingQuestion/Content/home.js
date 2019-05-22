71cf66002042648ab3bad8b6e9d7a7e317a78f13
salwan albahadly <albahadly@gmail.com>
2019-05-22 14:03:25 +1200

    Add project files.

---
+++ SymtechProgrammingQuestion/Content/home.js
@@ -0,0 +1,30 @@
+﻿
+
+$.fn.dateTimex =function () {
+    $("#datetime").focus(function () {
+        $("#myform input[type=text]").val('');
+        document.getElementById("datetime").value='';
+        $("#myform").show(500);
+
+
+        $.ajax({
+            url: '/api/date/',
+            type: 'GET',
+            dataType: 'json',
+            success: function (data) {
+                 document.getElementById("datetime").value = data.replace("T", " ");
+                console.log(data);
+            },
+            error: function () {
+                console.log('Error in Operation');
+            }
+        });
+
+    });
+
+};
+
+
+
+
+$("#dateTime").dateTimex();
---
+++ SymtechProgrammingQuestion/Controllers/DateController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Web.Http;
+
+namespace SymtechProgrammingQuestion.Controllers
+{
+    public class DateController : ApiController
+    {
+        public DateTime Get()
+        {
+            var random = new Random();
+            return new DateTime(2000, 1, 1)
+                .AddYears(random.Next(20))
+                .AddMonths(random.Next(12))
+                .AddHours(random.Next(24))
+                .AddMinutes(random.Next(60))
+                .AddSeconds(random.Next(60));
+        }
+
+        public void Post([FromBody]string value)
+        {
+            if (!DateTime.TryParseExact(value, "yyyy-MM-dd HH:mm:ss", CultureInfo.CurrentCulture, DateTimeStyles.AssumeUniversal, out var _))
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+        }
+    }
+}
